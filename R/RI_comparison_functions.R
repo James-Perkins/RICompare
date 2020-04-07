fc9eaf54c0cbd473a310abb41f83ce1c39b6e92e
James-Perkins <61221806+James-Perkins@users.noreply.github.com>
2020-04-07 13:29:55 +1000

    Add files via upload

--- R/RI_comparison_functions.R
+++ R/RI_comparison_functions.R
@@ -6,7 +6,7 @@
 #' @param polarity The polarity of the GC column. Defaults to "non-polar". Other option is "polar".
 #' @export
 #' @examples
-#' Get_kovats_RIs(Full_data = na.omit(Sample_data))
+#' Get_kovats_RIs(Sample_data)
 
 Get_kovats_RIs <- function(Full_data, type = "kovats", polarity = "non-polar"){
   Data <- Full_data[!duplicated(Full_data$CAS),]
@@ -34,6 +34,7 @@ Get_kovats_RIs <- function(Full_data, type = "kovats", polarity = "non-polar"){
 #' This function takes output of `nist_ri` or `Get_kovats_RIs` as an input and returns a matrix of CAS number, mean, standard deviation of the mean, and number of database samples.
 #' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
 #' @export
+#' @examples Summarise_RI_info(Sample_RI)
 
 Summarise_RI_info <- function(RI_data) {
   as.matrix(aggregate(RI ~ CAS,
@@ -45,53 +46,99 @@ Summarise_RI_info <- function(RI_data) {
                       } ))
   }
 
-#' Adds RI database matches to input dataframe
+#' Adds database RI values to input dataframe
 #'
-#' This function takes a raw dataframe of GC output and RI database search results from that dataframe and combines them into a new dataframe with no loss of information.
+#' This function takes a raw dataframe of GC output and RI database search results from that dataframe and combines them into a new dataframe no loss of information.
 #' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
 #' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
-#' @param Database_RI Specify the name of the column of database RI values to be compared. Defaults to "RI.mean" in line with `Summarise_RI_info`.
-#' @param Match_threshold The threshold at which a database RI value is considered matched with the calculated value. Defaults to 50.
 #' @export
+#' @examples Add_RI_data(Sample_data, Sample_RI)
 
-Matched_RI_data <- function(Full_data, RI_data, Database_RI = "RI.mean", Match_threshold = 50) {
-  RI_summaries <- Summarise_RI_info(Full_data)
+Add_RI_data <- function(Full_data, RI_data) {
+  RI_summaries <- Summarise_RI_info(RI_data)
   Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
-  New <- Combined[,c("CAS", "Name", "RI", Database_RI, "File")]
-  RI_comparison <- New[!is.na(New$RI.mean),]
-  RI_comparison$RI.mean <- as.numeric(paste(RI_comparison$RI.mean))
-  RI_comparison$Difference <- abs(RI_comparison$RI.mean-RI_comparison$RI)
-  All_data <- merge(Full_data, RI_comparison, all.x = TRUE)
-  Matched_RI <- na.omit(All_data[All_data$Difference < Match_threshold, ])
+  Combined$RI.mean <- as.numeric(paste(Combined$RI.mean))
+  Combined$Difference <- abs(Combined$RI.mean-Combined$RI)
+  Combined
 }
 
-#' All compounds with no database matches
+#' Makes dataframe of good RI database matches
+#'
+#' This function takes a raw dataframe of GC output and RI database search results from that dataframe and combines them into a new dataframe of well-matched compounds with no loss of columns.
+#' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
+#' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
+#' @param Match_threshold The threshold at which a database RI value is considered matched with the calculated value. Defaults to 50.
+#' @export
+#' @examples Match_RI_data(Sample_data, Sample_RI)
+
+Match_RI_data <- function(Full_data, RI_data, Match_threshold = 50) {
+  RI_summaries <- Summarise_RI_info(RI_data)
+  Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
+  Combined$RI.mean <- as.numeric(paste(Combined$RI.mean))
+  Combined$Difference <- abs(Combined$RI.mean-Combined$RI)
+  Onlymatches <- Combined[!is.na(Combined$Difference),]
+  Matched_RI <- Onlymatches[Onlymatches$Difference < Match_threshold, ]
+}
+
+#' Makes dataframe of poor RI database matches
+#'
+#' This function takes a raw dataframe of GC output and RI database search results from that dataframe and combines them into a new dataframe of poorly-matched compounds with no loss of columns..
+#' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
+#' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
+#' @param Match_threshold The threshold at which a database RI value is considered matched with the calculated value. Defaults to 50.
+#' @export
+#' @examples Poorly_matched_RI(Sample_data, Sample_RI)
+
+Poorly_matched_RI <- function(Full_data, RI_data, Match_threshold = 50) {
+  RI_summaries <- Summarise_RI_info(RI_data)
+  Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
+  Combined$RI.mean <- as.numeric(paste(Combined$RI.mean))
+  Combined$Difference <- abs(Combined$RI.mean-Combined$RI)
+  Onlymatches <- Combined[!is.na(Combined$Difference),]
+  Matched_RI <- Onlymatches[Onlymatches$Difference > Match_threshold, ]
+}
+
+#' Makes dataframe of compounds with no database matches
 #'
 #' This function is a subset of `Matched_RI_data`, but returns a dataframe of only the compounds with no matches.
 #' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
 #' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
-#' @param Database_RI Specify the name of the column of database RI values to be compared. Defaults to "RI.mean" in line with `Summarise_RI_info`.
 #' @export
+#' @examples No_matches(Sample_data, Sample_RI)
 
-No_database_matches <- function(Full_data, RI_data, Database_RI = "RI.mean"){
-  RI_summaries <- Summarise_RI_info(Full_data)
+No_matches <- function(Full_data, RI_data){
+  RI_summaries <- Summarise_RI_info(RI_data)
   Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
-  New <- Combined[,c("CAS", "Name", "RI", Database_RI, "File")]
-  no_database_RI_values <- New[is.na(New$RI.mean),]
+  RI_values <- Combined[is.na(Combined$RI.mean),]
+}
+
+#' All compounds with no or poor database matches
+#'
+#' This function is simply the inverse of `Match_RI_data`, and returns a dataframe of all compounds which are missing or are matched poorly.
+#' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
+#' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
+#' @param Threshold The threshold above which an RI match is considered poor. Defaults to 50.
+#' @export
+#' @examples Poor_or_no_matches(Sample_data, Sample_RI)
+
+Poor_or_no_matches <- function(Full_data, RI_data, Threshold = 50){
+  RI_summaries <- Summarise_RI_info(RI_data)
+  Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
+  Combined$RI.mean <- as.numeric(paste(Combined$RI.mean))
+  Combined$Difference <- abs(Combined$RI.mean-Combined$RI)
+  RI_values <- Combined[is.na(Combined$RI.mean)|Combined$Difference > Threshold,]
 }
 
 #' Only compounds with database matches
 #'
-#' This function is a subset of `Matched_RI_data`, but returns a dataframe of only the compounds with database matches.
+#' This function is a subset of `Add_RI_data`, but returns a dataframe of only the compounds with database information available, regardless of whether or not the match is good.
 #' @param Full_data A dataframe with at least one column of CAS numbers with column label "CAS", and one column of calculated RI values with column label "RI", one column of compound names called "Name",  and one column of sample identifiers called "File".
 #' @param RI_data The output of `nist_ri` or `Get_kovats_RIs`
-#' @param Database_RI Specify the name of the column of database RI values to be compared. Defaults to "RI.mean" in line with `Summarise_RI_info`.
 #' @export
+#' @examples Only_matches(Sample_data, Sample_RI)
 
-Only_database_matches <- function(Full_data, RI_data, Database_RI = "RI.mean"){
+Only_matches <- function(Full_data, RI_data){
   RI_summaries <- Summarise_RI_info(Full_data)
   Combined <- merge(Full_data, RI_summaries, all.x = TRUE)
-  New <- Combined[,c("CAS", "Name", "RI", Database_RI, "File")]
-  RI_matches <- New[!is.na(New$RI.mean),]
+  RI_matches <- Combined[!is.na(Combined$RI.mean),]
 }
-
